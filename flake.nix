8350e47ddbc84b4b0f0f75e2ada7a70e52e5301f
miracoly <68049792+miracoly@users.noreply.github.com>
2025-05-29 11:20:02 +0200

    build: support multiple systems & add bear shell script

--- flake.nix
+++ flake.nix
@@ -1,50 +1,64 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShell.${system} = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        bear
-        gcc
-        gnumake
-        gtest
-        sqlite
-      ];
-      shellHook = ''
-        bear -- make clean all
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      runBearMake = pkgs.writeShellScript "bear" ''
+        exec ${pkgs.bear}/bin/bear -- make clean all
       '';
-    };
-    packages.${system} = {
-      my-db = pkgs.stdenv.mkDerivation {
-        name = "my-db";
-        src = ./.;
-        buildInputs = [pkgs.gcc pkgs.gnumake];
-        nativeBuildInputs = [];
-        buildPhase = ''
-          make
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp main.out $out/bin/my-db
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          bear
+          gcc
+          gnumake
+          gtest
+          sqlite
+        ];
+        shellHook = ''
+          ${runBearMake}
         '';
       };
-      tests = pkgs.stdenv.mkDerivation {
-        name = "tests";
-        src = ./.;
-        buildInputs = with pkgs; [gcc gnumake gtest];
-        buildPhase = ''
-          make test.out
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp test.out $out/bin/tests
-        '';
+      apps = {
+        runBearMake = {
+          type = "app";
+          program = "${runBearMake}";
+        };
       };
-    };
-  };
+      packages = {
+        my-db = pkgs.stdenv.mkDerivation {
+          name = "my-db";
+          src = ./.;
+          buildInputs = [pkgs.gcc pkgs.gnumake];
+          nativeBuildInputs = [];
+          buildPhase = ''
+            make
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp main.out $out/bin/my-db
+          '';
+        };
+        tests = pkgs.stdenv.mkDerivation {
+          name = "tests";
+          src = ./.;
+          buildInputs = with pkgs; [gcc gnumake gtest];
+          buildPhase = ''
+            make test.out
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp test.out $out/bin/tests
+          '';
+        };
+      };
+    });
 }
